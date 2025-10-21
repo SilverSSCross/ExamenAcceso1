e724541f5f2639334f061b6eb2ee782d5c323893
Cross <joselidue@gmail.com>
2025-10-21 10:49:44 +0200

    Examen(Eliminar Placeholder)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         ServicioFiltradoPeliculas peliculas = new ServicioFiltradoPeliculas();
         //Demostracion filtrar por genero.(Crea el archivo vacio)
-        //peliculas.filtrarPorGenero("DASD");
+        peliculas.filtrarPorGenero("DASD");
         //Demostracion filtrar por genero
         peliculas.filtrarPorGenero("Drama");
         //Demostracion filtrar por director(Crea el archivo vacio)
-        //peliculas.filtrarPorDirector("Maito");
+        peliculas.filtrarPorDirector("Maito");
         //Demostracion filtrar por director
         peliculas.filtrarPorDirector("Christopher Nolan");
     }
